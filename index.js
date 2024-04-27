00529f6b645c76d7300d42a2f9a28a2b4e03263e
CallMePat <patienceemaiku@gmail.com>
2024-04-27 19:09:29 +0100

    Worked on the navbar

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-let lastScrollTop = 0;
 
-window.addEventListener("scroll", function() {
-  let currentScroll = window.scrollY || window.pageYOffset;
-  if (currentScroll > lastScrollTop) {
-    // scrolling down
-    document.querySelector(".container").classList.remove("navbar-white");
+// window.addEventListener("scroll", () => {
+//   const currentScroll = window.scrollY || window.pageYOffset;
+//   if (currentScroll > lastScrollTop) {
+//     // scrolling down
+//     document.querySelector(".container").classList.remove("navbar-white");
+//   } else {
+//     // scrolling up
+//     document.querySelector(".container").classList.add("navbar-white");
+//   }
+//   lastScrollTop = currentScroll;
+// }, false);
+
+
+window.addEventListener("scroll", () => {
+  const container =document.querySelector ('.container');
+
+  if (window.scrollY > 0) {
+    container.classList.add('scrolled')
   } else {
-    // scrolling up
-    document.querySelector(".container").classList.add("navbar-white");
+    container.classList.remove('scrolled')
   }
-  lastScrollTop = currentScroll;
-}, false);
+})
